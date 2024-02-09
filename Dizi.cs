307d1be9a4aec1f4d233b0e6946105ddfabbe2fb
irfan ayaz <byirfanayaz@gmail.com>
2024-02-09 23:37:02 +0300

    Dizilerde En büyük En küçük eleman bulma

---
+++ Dizi.cs
@@ -0,0 +1,43 @@
+﻿namespace Diziler;
+
+public class Dizi
+{
+    public int[] Olustur(int limit)
+    {
+    int[] sayilar = new int[limit];
+        for (int i = 0; i < limit; i++)
+        {
+            sayilar[i] = new Random().Next(1, 100);
+            Console.Write("{0,5}", sayilar[i]);
+        }
+        return sayilar;
+    }    
+        
+    
+
+   public static int EnBuyuk(int[] dizi)
+        {
+            int eb = dizi[0];
+            for (int i = 1; i < dizi.Length; i++)
+            {
+                if (dizi[i]>eb)
+                {
+                    eb = dizi[i];
+                }
+            }
+            return eb;
+        }
+        public static int EnKucuk(int[] dizi)
+        {
+            int ek = dizi[0];
+            for (int i = 1; i < dizi.Length; i++)
+            {
+                if (dizi[i]<ek)
+                {
+                    ek = dizi[i];
+                }
+            }return ek;
+        }     
+
+
+}
